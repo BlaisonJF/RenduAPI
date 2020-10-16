587fd6bd7e47ae141b6b5a69863b5058cac3de68
BlaisonJF <71656253+BlaisonJF@users.noreply.github.com>
2020-10-16 22:15:56 +0200

    Update APIcours12octobre.py

--- APIcours12octobre.py
+++ APIcours12octobre.py
@@ -5,9 +5,13 @@ Created on Tue Oct 13 11:40:44 2020
 @author: jfbla
 """
 
-import requests
+import requests #dépend code java
+
+produit = '{"code":5,"designation":"SFP_module","prix":200}'
+produit_url = "http://localhost:8989/produit"+code
+ajouterunproduit = "http://localhost:8989/ajouterunproduit"+produit
 
 r = requests.get('http://localhost:8989/produit/')
-	#récupère les infos (produits) à l'adresse indiquée ci-dessus
+	#récupère les infos sur les produits à l'adresse indiquée ci-dessus
 print(r)
 	#j'affiche les produits dans la console
